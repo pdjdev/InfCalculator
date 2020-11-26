66fecc83c109628a278352303090be52a167eff0
pdjdev <pdj5096@gmail.com>
2020-11-26 22:36:54 +0900

    main func

--- InfCalculator/main.c
+++ InfCalculator/main.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
+    char s[255];
+
+    printf("======= Welcome to infinite calculator! =======\n"
+           "Enter the expression in infix notation.\n"
+           "Input : ");
+    scanf("%s", s);
+    printf("Output : %s", s);
+
     return 0;
 }

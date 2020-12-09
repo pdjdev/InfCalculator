9569a9c7a63a23a33275f12f3e331acef35d8e18
Dongjun Park <pdj5096@hanyang.ac.kr>
2020-12-09 21:54:20 +0900

    Update task.h

--- InfCalculator/task.h
+++ InfCalculator/task.h
@@ -1,1 +1,16 @@
+void GetExpr() {
+    FILE *fq = fopen("input", "r"); 
+    if(fq == NULL){
+        printf("Input file not found.\n");
+        exit(0);
+    }
 
+    char input = getc(fq);
+
+    if(input=='\n'){
+        printf("Input file is empty, or starts with newline.\n");
+        exit(0);
+    }
+
+    fclose(fq);
+}

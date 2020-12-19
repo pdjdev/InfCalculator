898b0875a82a653cbed1dba3cd030125e078ce7e
(no1gun2) <(no1gun2@naver.com)>
2020-12-19 15:48:33 +0900

    commit test

--- InfCalculator/calc.c
+++ InfCalculator/calc.c
@@ -1,4 +1,5 @@
-LINK plus(LINK a, LINK b) {
+LINK plus(LINK a, LINK b)
+{
 
     LINK ans;
     LINK num_copy;
@@ -9,7 +10,8 @@ LINK plus(LINK a, LINK b) {
     char now = 0;
     char up = 0;
 
-    if(compare(a,b) == 2) {
+    if (compare(a, b) == 2)
+    {
         LINK tmp = a;
         a = b;
         b = tmp;
@@ -17,58 +19,78 @@ LINK plus(LINK a, LINK b) {
 
     positive = (a->d == '+');
 
-    num1 = a->next; num1->prev = NULL; free(a);
-    num2 = b->next; num2->prev = NULL; free(b);
-    
-    num2 = Upzero_fill(num1,num2);
+    num1 = a->next;
+    num1->prev = NULL;
+    free(a);
+    num2 = b->next;
+    num2->prev = NULL;
+    free(b);
 
-    a = num1; num1 = char_to_list('0'); concatenate(num1, a);
-    a = num2; num2 = char_to_list('0'); concatenate(num2, a);
+    num2 = Upzero_fill(num1, num2);
 
-    Downzero_fill(num1,num2);
+    a = num1;
+    num1 = char_to_list('0');
+    concatenate(num1, a);
+    a = num2;
+    num2 = char_to_list('0');
+    concatenate(num2, a);
+
+    Downzero_fill(num1, num2);
 
     a = last_link(num1);
     b = num2;
-    for(; a->d != '.'; a=a->prev) point_count++; del_link(a);
-    for(; b->d != '.'; b=b->next); del_link(b);
- 
+    for (; a->d != '.'; a = a->prev)
+        point_count++;
+    del_link(a);
+    for (; b->d != '.'; b = b->next)
+        ;
+    del_link(b);
+
     a = last_link(num1);
     b = last_link(num2);
 
-    while(a!=NULL){
-        char number1 = a->d-'0';
-        char number2 = b->d-'0';
+    // 계신
+    while (a != NULL)
+    {
+        char number1 = a->d - '0';
+        char number2 = b->d - '0';
 
         now = number1 + number2 + up;
-        
-        up = now/10;
-        a->d = now%10 + '0';
+
+        up = now / 10;
+        a->d = now % 10 + '0';
         a = a->prev;
         b = b->prev;
     }
-    
+
     ans = copy_link(num1);
 
-    free_all(num1); free_all(num2);
+    free_all(num1);
+    free_all(num2);
 
     a = last_link(ans);
-    while(point_count--) a = a->prev;
-    insert(a,'.');
+    while (point_count--)
+        a = a->prev;
+    insert(a, '.');
 
     zero_erase(ans);
 
     a = ans;
     ans = char_to_list(positive ? '+' : '-');
-    concatenate(ans,a);
+    concatenate(ans, a);
 
     a = ans->next;
-    while(a->next->d != '.' && a->d == '0') { del_link(ans->next); a=a->next; }
+    while (a->next->d != '.' && a->d == '0')
+    {
+        del_link(ans->next);
+        a = a->next;
+    }
 
     return ans;
 }
 
-
-LINK minus(LINK a, LINK b) {
+LINK minus(LINK a, LINK b)
+{
 
     LINK ans;
     LINK num_copy;
@@ -76,21 +98,24 @@ LINK minus(LINK a, LINK b) {
     LINK num2;
     bool positive;
     char big_num;
-    unsigned long long int point_count = 0; 
+    unsigned long long int point_count = 0;
     char now = 0;
     char down = 0;
 
-    big_num = compare(a,b);
+    big_num = compare(a, b);
 
-    if(big_num == 3){ // 둘의 절대값이 같을 때
+    if (big_num == 3)
+    { // 둘의 절대값이 같을 때
         ans = char_to_list('+');
-        insert(ans,'.');
-        insert(ans,'0');
-        free_all(a); free_all(b);
+        insert(ans, '.');
+        insert(ans, '0');
+        free_all(a);
+        free_all(b);
         return ans;
     }
-    
-    if(big_num == 2) { // b가 더 크면 b가 a로 이동
+
+    if (big_num == 2)
+    { // b가 더 크면 b가 a로 이동
         num_copy = a;
         a = b;
         b = num_copy;
@@ -98,56 +123,72 @@ LINK minus(LINK a, LINK b) {
 
     positive = (a->d == '+');
 
-    num1 = a->next; num1->prev = NULL; free(a);
-    num2 = b->next; num2->prev = NULL; free(b);
-    
-    num2 = Upzero_fill(num1,num2);
+    num1 = a->next;
+    num1->prev = NULL;
+    free(a);
+    num2 = b->next;
+    num2->prev = NULL;
+    free(b);
 
-    Downzero_fill(num1,num2);
+    num2 = Upzero_fill(num1, num2);
+
+    Downzero_fill(num1, num2);
 
     a = last_link(num1);
     b = num2;
-    for(; a->d != '.'; a=a->prev) point_count++; del_link(a);
-    for(; b->d != '.'; b=b->next); del_link(b);
+    for (; a->d != '.'; a = a->prev)
+        point_count++;
+    del_link(a);
+    for (; b->d != '.'; b = b->next)
+        ;
+    del_link(b);
 
     a = last_link(num1);
     b = last_link(num2);
 
-    while(a!=NULL) {
-        char number1 = a->d-'0';
-        char number2 = b->d-'0';
+    while (a != NULL)
+    {
+        char number1 = a->d - '0';
+        char number2 = b->d - '0';
         char check = 0;
-        if(number2-down > number1) {number1 += 10; check = 1;};
+        if (number2 - down > number1)
+        {
+            number1 += 10;
+            check = 1;
+        };
         now = number1 - number2 + down;
         down = check ? -1 : 0;
         a->d = now + '0';
         a = a->prev;
         b = b->prev;
     }
-    
+
     ans = copy_link(num1);
-    free_all(num1); free_all(num2);
+    free_all(num1);
+    free_all(num2);
 
     a = last_link(ans);
-    while(point_count--) a = a->prev;
-    insert(a,'.');
-    
+    while (point_count--)
+        a = a->prev;
+    insert(a, '.');
+
     zero_erase(ans);
-    
+
     a = ans;
     ans = char_to_list(positive ? '+' : '-');
-    concatenate(ans,a);
+    concatenate(ans, a);
 
     a = ans->next;
-    for(; a->next->d != '.' && a->d == '0'; a=a->next) del_link(ans->next);
+    for (; a->next->d != '.' && a->d == '0'; a = a->next)
+        del_link(ans->next);
 
     return ans;
 }
 
+LINK multiple(LINK num1, LINK num2)
+{
 
-LINK multiple(LINK num1, LINK num2) {
-
-    LINK ans; 
+    LINK ans;
     LINK num_copy;
     LINK num1_last = last_link(num1);
     LINK num2_last = last_link(num2);
@@ -161,69 +202,89 @@ LINK multiple(LINK num1, LINK num2) {
     bool positive;
 
     positive = (num1->d == num2->d);
- 
+
     unsigned long long point = 0;
     num_copy = num1_last;
-    for( ; num_copy->d != '.' ; num_copy = num_copy->prev) point++;//3 
+    for (; num_copy->d != '.'; num_copy = num_copy->prev)
+        point++; //3
     num_copy = num2_last;
-    for( ; num_copy->d != '.' ; num_copy = num_copy->prev) point++;//3+4=7
+    for (; num_copy->d != '.'; num_copy = num_copy->prev)
+        point++; //3+4=7
 
     num_copy = num1_int;
-    for( ; num_copy->d != '.'; num_copy = num_copy->next); del_link(num_copy);
+    for (; num_copy->d != '.'; num_copy = num_copy->next)
+        ;
+    del_link(num_copy);
     num_copy = num2_int;
-    for( ; num_copy->d != '.'; num_copy = num_copy->next); del_link(num_copy);
+    for (; num_copy->d != '.'; num_copy = num_copy->next)
+        ;
+    del_link(num_copy);
 
     mul1 = last_link(num1_int);
     unsigned long long int zero = 0;
-    for(; mul1 != NULL; mul1 = mul1->prev) {
+    for (; mul1 != NULL; mul1 = mul1->prev)
+    {
         mul2 = last_link(num2_int);
-        char now = (mul1->d-'0') * (mul2->d-'0'); 
-        char up = now/10;
-        now %=10;
+        char now = (mul1->d - '0') * (mul2->d - '0');
+        char up = now / 10;
+        now %= 10;
 
         input = char_to_list(now + '0');
 
         num_copy = input;
         mul2 = mul2->prev;
 
-        for(; mul2 != NULL; mul2 = mul2->prev) {
-            now = (mul1->d-'0') * (mul2->d-'0') + up;
-            up = now/10;
+        for (; mul2 != NULL; mul2 = mul2->prev)
+        {
+            now = (mul1->d - '0') * (mul2->d - '0') + up;
+            up = now / 10;
             now %= 10;
             insert(num_copy, now + '0');
             num_copy = num_copy->next;
         }
 
-        if(up != 0) { insert(num_copy, up+'0'); }
-        num_copy = last_link(input);
-        for( ; num_copy->prev != NULL; num_copy = num_copy->prev) {
-            if(num_copy->d != '0') break;
+        if (up != 0)
+        {
+            insert(num_copy, up + '0');
         }
-        
-        if(zero>0){
-            zero_link = char_to_list('0');
-            for(unsigned long long i=1; i<zero; i++) {
-                insert(zero_link,'0');
-            }
-            concatenate(zero_link,input);
-            input = zero_link;
-        } 
-        
         num_copy = last_link(input);
-        while(1) {
-            if(num_copy->prev == NULL || num_copy->d != '0') break;
+        for (; num_copy->prev != NULL; num_copy = num_copy->prev)
+        {
+            if (num_copy->d != '0')
+                break;
+        }
+
+        if (zero > 0)
+        {
+            zero_link = char_to_list('0');
+            for (unsigned long long i = 1; i < zero; i++)
+            {
+                insert(zero_link, '0');
+            }
+            concatenate(zero_link, input);
+            input = zero_link;
+        }
+
+        num_copy = last_link(input);
+        while (1)
+        {
+            if (num_copy->prev == NULL || num_copy->d != '0')
+                break;
             LINK save = num_copy->prev;
             del_link(num_copy);
             num_copy = save;
         }
 
-        if(!zero) ans = copy_link(input);
-        
-        else {
+        if (!zero)
+            ans = copy_link(input);
+
+        else
+        {
             unsigned long long alen = count(ans);
             unsigned long long blen = count(input);
 
-            if(blen>alen){
+            if (blen > alen)
+            {
                 num_copy = ans;
                 ans = input;
                 input = num_copy;
@@ -232,79 +293,115 @@ LINK multiple(LINK num1, LINK num2) {
             num_copy = input;
             now = 0;
             up = 0;
-            for(; num_copy != NULL; num_copy = num_copy->next) {
-                now = num_copy->d-'0' + save->d-'0' +up;
-                up = now/10;
-                now = now%10;
-                save->d = now+'0';
+            for (; num_copy != NULL; num_copy = num_copy->next)
+            {
+                now = num_copy->d - '0' + save->d - '0' + up;
+                up = now / 10;
+                now = now % 10;
+                save->d = now + '0';
                 save = save->next;
             }
-            for(; up && save != NULL; save = save->next) {
-                now = save->d-'0' + up;
-                up = now/10;
-                now = now%10;
-                save->d = now+'0';
+            for (; up && save != NULL; save = save->next)
+            {
+                now = save->d - '0' + up;
+                up = now / 10;
+                now = now % 10;
+                save->d = now + '0';
             }
-            if(up) {
+            if (up)
+            {
                 save = last_link(ans);
-                insert(save,'1');
+                insert(save, '1');
             }
         }
         free_all(input);
-        zero++; 
+        zero++;
     }
-    
+
     unsigned long long anslen = count(ans);
-    if(anslen > point) {
+    if (anslen > point)
+    {
         num_copy = ans;
         save = ans;
-        if(positive) ans = char_to_list('+');
-        else ans = char_to_list('-');
+        if (positive)
+            ans = char_to_list('+');
+        else
+            ans = char_to_list('-');
 
-        for(; num_copy!=NULL; num_copy = num_copy->next) { insert(ans,num_copy->d); }
+        for (; num_copy != NULL; num_copy = num_copy->next)
+        {
+            insert(ans, num_copy->d);
+        }
         free_all(save);
 
         num_copy = last_link(ans);
-        while(point--) num_copy = num_copy->prev;
+        while (point--)
+            num_copy = num_copy->prev;
         insert(num_copy, '.');
-    } else {
+    }
+    else
+    {
         num_copy = ans;
-        if(positive) ans = char_to_list('+');
-        else ans = char_to_list('-');
-        save = ans;        
-        insert(save,'0'); save = save->next; insert(save,'.'); save = save->next;
-        point-=anslen;
-        while(point--){
-            insert(save,'0');
+        if (positive)
+            ans = char_to_list('+');
+        else
+            ans = char_to_list('-');
+        save = ans;
+        insert(save, '0');
+        save = save->next;
+        insert(save, '.');
+        save = save->next;
+        point -= anslen;
+        while (point--)
+        {
+            insert(save, '0');
             save = save->next;
         }
-        for(; num_copy!=NULL; num_copy = num_copy->next) {
-            insert(save,num_copy->d);
+        for (; num_copy != NULL; num_copy = num_copy->next)
+        {
+            insert(save, num_copy->d);
         }
     }
     zero_erase(ans);
-    free_all(num1);     free_all(num2);
-    free_all(num1_int); free_all(num2_int);
+    free_all(num1);
+    free_all(num2);
+    free_all(num1_int);
+    free_all(num2_int);
     return ans;
 }
 
-LINK calculator(LINK p1, LINK p2, char oper) {
+LINK calculator(LINK p1, LINK p2, char oper)
+{
     LINK p3;
     extern bool printlog;
 
-    if((oper == '+' || oper=='-' || oper == '*') && printlog) {
+    if ((oper == '+' || oper == '-' || oper == '*') && printlog)
+    {
         extern int calccount;
         printf("%d\t", ++calccount);
-        print_link(p1); printf("\t\t");
-        print_link(p2); printf("\t\t%c\n", oper);
+        print_link(p1);
+        printf("\t\t");
+        print_link(p2);
+        printf("\t\t%c\n", oper);
     }
 
-    if(oper == '+' || oper=='-') {
-        if(oper == '-') { p2->d = (p2->d == '-' ? '+' : '-'); }
-        if(p1->d + p2->d == '+'+'+' || p1->d + p2->d == '-'+'-') { p3 = plus(p1,p2); }
-        else { p3 = minus(p1,p2); }
+    if (oper == '+' || oper == '-')
+    {
+        if (oper == '-')
+        {
+            p2->d = (p2->d == '-' ? '+' : '-');
+        }
+        if (p1->d + p2->d == '+' + '+' || p1->d + p2->d == '-' + '-')
+        {
+            p3 = plus(p1, p2);
+        }
+        else
+        {
+            p3 = minus(p1, p2);
+        }
     }
 
-    if(oper == '*') p3 = multiple(p1,p2);
+    if (oper == '*')
+        p3 = multiple(p1, p2);
     return p3;
 }

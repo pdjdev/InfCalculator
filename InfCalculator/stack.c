2a2d0877fd508723842aec9be2657030be9a8f12
yumin0411 <62548846+yumin0411@users.noreply.github.com>
2020-12-20 22:07:28 +0900

    Update stack.c

--- InfCalculator/stack.c
+++ InfCalculator/stack.c
@@ -1,29 +1,35 @@
+//스택 탑에 NULL로 초기화
 #define InitStack(STACK)    (STACK)->top = NULL
 
-typedef struct Node{ //
+//노드 정의
+typedef struct Node{
     LINK data;
     struct Node *next;
 }Node;
 
+//스택 정의
 typedef struct Stack{
     Node *top;
 }Stack;
 
+//스택 탑이 가리키는 것이 있는지 없는지 리턴
 bool IsEmpty(Stack *stack){
     return stack->top == NULL;
 }
 
+//헤드를 스택 탑으로 푸시
 void Push(Stack *stack, LINK head){
-    Node *now= (Node*)malloc(sizeof(Node));
+    Node *now= (Node*)malloc(sizeof(Node)); //now의 메모리를 노드크기로 동적할당
     now->data = head; 
     now->next = stack->top;
     stack->top = now;
 }
 
+//스택 탑을 팝
 LINK Pop(Stack *stack){
     Node *now;
     LINK re;
-    if(IsEmpty(stack)) return NULL;
+    if(IsEmpty(stack)) return NULL; //
     now = stack->top;
     re = now->data;
 
@@ -31,13 +37,15 @@ LINK Pop(Stack *stack){
     return re;
 }
 
+//
 LINK Top(Stack *stack){
     return stack->top->data;
 }
 
+//
 void stack_reset(Stack *stack){
     Node *now = stack->top;
-    while(now != NULL){
+    while(now != NULL){ //
         Node *next = now->next;
         free(now);
         now = next;
